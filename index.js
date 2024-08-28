8ad97ced697c71bdce662254a861e764c8c301ac
MhAhmadAli <fa17-bsse-023@lgu.edu.pk>
2024-08-28 13:59:21 +0500

    bodyparser

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const express = require('express');
-var cors = require('cors')
-const fs = require('fs');
-const path = require('path');
-const app = express();
+const bodyParser = require('body-parser');
+const multer = require('multer');
+var cors = require('cors');
 
-app.use(cors())
+const app = express();
+const upload = multer();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(upload.array());
 
 app.get('/', (req, res) => {
   console.log("Data Request")
   return res.send('hello, World!');
-});
-
-app.get('/myclass', (req, res) => {
-  console.log("Data Request")
-  return res.send('myclass!');
 });
 
 app.post('/', (req, res) => {
@@ -28,6 +28,15 @@ app.delete('/', (req, res) => {
   return res.send('Delete Called!');
 });
 
+app.post('/login', (req, res) => {
+  console.log(req.body);
+  res.send('Hello World!');
+});
+
+app.post('/signup', (req, res) => {
+  res.send("Signup Success");
+})
+
 app.listen(4000, () => {
-  console.log("Server started!")
+  console.log("Server started on http://localhost:4000")
 });
