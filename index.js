bdd0899a8664946fb41e96db62fc631d5c2518d2
MhAhmadAli <fa17-bsse-023@lgu.edu.pk>
2024-08-30 12:55:10 +0500

    login signup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const multer = require('multer');
+const fs = require('fs');
 var cors = require('cors');
 
 const app = express();
@@ -29,12 +30,26 @@ app.delete('/', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-  console.log(req.body);
-  res.send('Hello World!');
+  const {email, pass} = req.body;
+
+  const userData = JSON.parse(fs.readFileSync('user.json'));
+
+  setTimeout(() => {
+    if(email !== userData.email && pass !== userData.pass) {
+      return res.status(401).send("Wrong Credentials");
+    }
+    return res.status(200).send("Logged in Successfully");
+  }, 5000);
 });
 
 app.post('/signup', (req, res) => {
-  res.send("Signup Success");
+  fs.writeFile('user.json', JSON.stringify(req.body), (err) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send('Error Saving Data');
+    }
+    return res.status(200).send("Data Saved Successfully");
+  });
 })
 
 app.listen(4000, () => {
