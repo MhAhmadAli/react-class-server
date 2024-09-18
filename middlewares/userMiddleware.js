1ab2dc88cfae3949527d953af6ab181844cb13cc
MhAhmadAli <fa17-bsse-023@lgu.edu.pk>
2024-09-18 13:40:22 +0500

    validation middlewares

--- middlewares/userMiddleware.js
+++ middlewares/userMiddleware.js
@@ -5,6 +5,31 @@ function isAuthorized(req, res, next) {
   next();
 }
 
+function checkEmail(req, res, next) {
+  if(!req.body.email) {
+    return res.status(400).send("Email is required!");
+  }
+  let temp  = req.body.email;
+  if (!temp.includes("@")) {
+    return res.status(400).send("Invalid Email!");
+  }
+  next();
+}
+
+function checkPassword(req, res, next) {
+  if(!req.body.pass) {
+    return res.status(400).send("Password is required!");
+  }
+  let temp = req.body.pass;
+  if (temp.length < 10) {
+    return res.status(400).send("Password must be 10 characters long!");
+  }
+
+  next();
+}
+
 module.exports = {
-  isAuthorized
+  isAuthorized,
+  checkEmail,
+  checkPassword
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,8 +3,8 @@ const userController = require("../controller/userController");
 const userMiddleware = require("../middlewares/userMiddleware");
 const router = express.Router();
 
-router.post('/login', userController.login);
-router.post('/signup', userController.signup);
+router.post('/login', userMiddleware.checkEmail, userMiddleware.checkPassword, userController.login);
+router.post('/signup', userMiddleware.checkEmail, userMiddleware.checkPassword, userController.signup);
 router.get("/getUser", userMiddleware.isAuthorized, userController.getUser);
 router.get("/getUser/:email", userController.dynamicEmail);
 
